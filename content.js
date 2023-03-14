ee20155696eead212128f6fe085255e51960a911
ArthurDuino <82079353+arthurduino@users.noreply.github.com>
2023-03-14 10:06:01 +0100

    Update content.js
    Permet la sélection du texte pour les articles payants

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ function removeElements(selector) {
    let element = document.querySelector('div.article-full__body-content.article-paywall');
    if (element) {
      element.style.maxHeight = '5000rem';
+     element.dataset.selection = 'enabled'
    }
  }
  
@@ -15,5 +16,3 @@ function removeElements(selector) {
  removeElements('div.subscription-block');
  removeElements('div.aside__item.aside-newspaper');
  removeElements('a.navbar-top__nav-btn.btn.btn--sub');
- 
- 
